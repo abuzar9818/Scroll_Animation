5a316c52e09b0f5512a09bbd8f4ef3abe37ab600
abuzar9818 <abuzarahmad678@gmail.com>
2026-03-11 04:36:49 +0530

    feat: Add `basePath` and `assetPrefix` to Next.js configuration.

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  basePath: "/Scroll_Animation",
+  assetPrefix: "/Scroll_Animation/",
 };
 
 export default nextConfig;
